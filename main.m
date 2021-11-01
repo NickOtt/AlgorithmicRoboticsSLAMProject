9cf827aa3ba17fd85efc28ca90f402bace6d15dc
Ethan Frank <edf32@case.edu>
2021-11-01 16:05:53 -0400

    Replace mu_bar with mu

--- main.m
+++ main.m
@@ -65,7 +65,7 @@ commanded_poses = [mu];
 
 for i=1:length(commands)
     % Step 1: Predict current state forward
-    [mu_bar, P_bar] = ekf_prediction(mu, P, commands(:, i), alpha);
+    [mu, P] = ekf_prediction(mu, P, commands(:, i), alpha);
 
     % Simulate motion (This is where the robot "actually" is)
     real_pos = sample_motion_model_velocity(commands(:,i),real_poses(:, end),alpha);
@@ -83,13 +83,13 @@ for i=1:length(commands)
     hits = ~isnan(noisy_scan);
     
     % Convert scan to xy coordinates based on where robot "thinks" it is
-    coords = scan_to_xy(noisy_scan(hits), lidar.scanAngles(hits), mu_bar);
+    coords = scan_to_xy(noisy_scan(hits), lidar.scanAngles(hits), mu);
         
     % Extract lines using ransac
     lines = multi_line_ransac(coords, N, dist, L, n_thresh);
     
     % Plot visualization
-    plot_world(mu_bar, P_bar, coords, map, lines, real_poses, filtered_poses, commanded_poses, []);
+    plot_world(mu, P, coords, map, lines, real_poses, filtered_poses, commanded_poses, []);
     pause;
 
     % Find the closest points on each line to a given global position
@@ -103,7 +103,7 @@ for i=1:length(commands)
         measured_landmarks(:, j) = closest_point_on_line(lines(:, j), pos);
     end
     
-    plot_world(mu_bar, P_bar, coords, map, lines, real_poses, filtered_poses, commanded_poses, measured_landmarks);
+    plot_world(mu, P, coords, map, lines, real_poses, filtered_poses, commanded_poses, measured_landmarks);
     pause;
     
     % Exctract the landmarks from the current state. Associate measured
@@ -111,8 +111,8 @@ for i=1:length(commands)
     % are those in the measured_landmarks array with matched_indices == 0
     % Landmakrs covs are extracted from matrix P. Potentially, they should
     % be a combination of the robot cov and the landmark cov. 
-    landmarks = reshape(mu_bar(4:end), 2, []);
-    landmark_covs = P_bar;
+    landmarks = reshape(mu(4:end), 2, []);
+    landmark_covs = P;
 
     [matched_indices, landmark_counts] = associate_landmarks(landmarks, measured_landmarks, landmark_covs, landmark_counts, lambda);
     
@@ -121,30 +121,30 @@ for i=1:length(commands)
     % Run update using detected landmarks to update robot and landmarks
     for j = 1:size(measured_landmarks, 2)
         if matched_indices(j) ~= 0
-            z_actual = landmark_measurement(measured_landmarks(:,j), mu_bar(1:3));
-            [mu_bar, P_bar] = ekf_correction(mu_bar, P_bar, z_actual, matched_indices(j), sigma_r, sigma_phi);
-            plot_world(mu_bar, P_bar, coords, map, lines, real_poses, filtered_poses, commanded_poses, measured_landmarks);
+            z_actual = landmark_measurement(measured_landmarks(:,j), mu(1:3));
+            [mu, P] = ekf_correction(mu, P, z_actual, matched_indices(j), sigma_r, sigma_phi);
+            plot_world(mu, P, coords, map, lines, real_poses, filtered_poses, commanded_poses, measured_landmarks);
             pause;
         end
     end
     
-    plot_world(mu_bar, P_bar, coords, map, lines, real_poses, filtered_poses, commanded_poses, measured_landmarks);
+    plot_world(mu, P, coords, map, lines, real_poses, filtered_poses, commanded_poses, measured_landmarks);
     pause;
 
     % Add newly detected landmarks to the array
     for j = 1:size(measured_landmarks, 2)
         if matched_indices(j) == 0
-            z_actual = landmark_measurement(measured_landmarks(:,j), mu_bar(1:3));
-            [mu_bar, P_bar] = ekf_add_landmark(mu_bar, P_bar, z_actual, sigma_r, sigma_phi);
+            z_actual = landmark_measurement(measured_landmarks(:,j), mu(1:3));
+            [mu, P] = ekf_add_landmark(mu, P, z_actual, sigma_r, sigma_phi);
         end
     end
     
-    mu = mu_bar;
-    P = P_bar;
+    mu = mu;
+    P = P;
     
     % Update filtered poses history
     filtered_poses = [filtered_poses, mu(1:3)];
     
-    plot_world(mu_bar, P_bar, coords, map, lines, real_poses, filtered_poses, commanded_poses, []);
+    plot_world(mu, P, coords, map, lines, real_poses, filtered_poses, commanded_poses, []);
     pause;
 end
